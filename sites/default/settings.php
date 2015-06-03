00013531291a99d6a63d1fe8d60e885af7b79ff0
Abeer Khakwani & chitra atmakuru & Kyle Giard-Chase <student@epicodus.com>
2015-06-03 16:56:15 -0700

    add stuffs

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'food',
       'username' => 'food',
       'password' => 'food',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
